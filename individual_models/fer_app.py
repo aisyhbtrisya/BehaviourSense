71f829088afef6f3e2dc8e7a6538673d0d63fa9d
donghycksun <aisyahbatrisya66@gmail.com>
2026-06-22 00:21:17 +0800

    Update fer_app.py

--- individual_models/fer_app.py
+++ individual_models/fer_app.py
@@ -23,15 +23,15 @@ emotion_labels = {
 }
 
 # FER2013 labels
-emotion_labels = {
-    0: "angry",
-    1: "disgust",
-    2: "fear",
-    3: "happy",
-    4: "neutral",
-    5: "sad",
-    6: "surprise"
-}
+#emotion_labels = {
+#    0: "angry",
+#    1: "disgust",
+#    2: "fear",
+#    3: "happy",
+#    4: "neutral",
+#    5: "sad",
+#    6: "surprise"
+#}
 
 st.title("FER - Facial Emotion Recognition")
 
