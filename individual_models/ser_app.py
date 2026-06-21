be52c3829f2efdfee26f91de35280b0a6169c00b
donghycksun <aisyahbatrisya66@gmail.com>
2026-06-22 03:58:41 +0800

    update

--- individual_models/ser_app.py
+++ individual_models/ser_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import librosa
 import keras
-from tensorflow.keras.models import load_model
+from tf_keras.models import load_model
 
 @st.cache_resource
 
